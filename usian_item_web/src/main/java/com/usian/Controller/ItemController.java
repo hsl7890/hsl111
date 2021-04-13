ff2206f9a491edce733c56a20480685bf53cc70a
hsl7890 <1914088365@qq.com>
2021-04-13 11:31:33 +0800

    two

--- usian_item_web/src/main/java/com/usian/Controller/ItemController.java
+++ usian_item_web/src/main/java/com/usian/Controller/ItemController.java
@@ -2,24 +2,78 @@ package com.usian.Controller;
 
 import com.usian.feign.ItemServiceFeign;
 import com.usian.pojo.TbItem;
+import com.usian.utils.PageResult;
 import com.usian.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping("frontend/item")
+@RequestMapping("backend/item")
 public class ItemController {
 
     @Autowired
     private ItemServiceFeign itemServiceFeign;
 
     @RequestMapping("selectItemInfo")
-    public Result selectItemInfo(Long itemId){
+    public Result selectItemInfo(Long itemId) {
         TbItem tbItem = itemServiceFeign.selectItemInfo(itemId);
-        if(tbItem != null){
+        if (tbItem != null) {
             return Result.ok(tbItem);
         }
-        return Result.error("青梅配绿茶 天生是一对");
+        return Result.error("弄啥嘞。。。。。");
+    }
+
+    @RequestMapping("selectTbItemAllByPage")
+    public Result selectTbItemAllByPage(@RequestParam(defaultValue = "1") Integer page,
+                                        @RequestParam(defaultValue = "10") Integer rows) {
+        PageResult pageResult = itemServiceFeign.selectTbItemAllByPage(page, rows);
+        if(pageResult != null){
+            return Result.ok(pageResult);
+        }
+        return Result.error("查都查不出来啊");
+    }
+
+    @RequestMapping("insertTbItem")
+    public Result insertTbItem(TbItem tbItem, String desc, String itemParams){
+        Integer count = itemServiceFeign.insertTbItem(tbItem, desc, itemParams);
+        if(count == 3){
+            return Result.ok();
+        }
+        return Result.error("添加失败");
+    }
+
+    @RequestMapping("deleteItemById")
+    public Result deleteItemById(@RequestParam(defaultValue = "0") Long itemId){
+        Integer count = itemServiceFeign.deleteItemById(itemId);
+        if (count>0) {
+            return Result.ok();
+        }
+        return Result.error("删除失败");
+    }
+
+
+
+
+    @RequestMapping("preUpdateItem")
+    public Result preUpdateItem(Long itemId){
+        Map<String, Object> map = itemServiceFeign.preUpdateItem(itemId);
+        if(map != null){
+            return Result.ok(map);
+        }
+        return Result.error("查询失败");
+    }
+
+
+    @RequestMapping("updateTbItem")
+    public Result updateTbItem( TbItem tbItem, String desc, String itemParams){
+        Integer integer = itemServiceFeign.updateTbItem(tbItem, desc, itemParams);
+        if(integer==3){
+            return Result.ok();
+        }
+        return Result.error("修改失败 ");
     }
 }
